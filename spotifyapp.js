9de20bbe201f3f8c671b40935b19b1d491cf91ac
dw <davidwhitebird@gmail.com>
2018-07-17 16:03:30 -0500

    remove prior path, config

--- spotifyapp.js
+++ spotifyapp.js
@@ -14,8 +14,8 @@ if (process.env.NODE_ENV !== 'production') {
 
 //    # DIFFERENT VALUES IN DEV & PRODUCTION
 const port = process.env.PORT || 3333; // checks local environment for port assignment, if none than use the assigned.
-var redirect_uri = process.env.DV_SPOTIFY_REDIRECT_URI; // Your redirect uri
-var ui_server = process.env.DV_UI_SERVER;
+var redirect_uri = process.env.SPOTIFY_REDIRECT_URI; // Your redirect uri
+var ui_server = process.env.UI_SERVER;
 
 //    # SAME VALUES IN DEV & PRODUCTION
 var client_id = process.env.SPOTIFY_CLIENT_ID; // Your client id
